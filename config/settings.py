0b2a9b7baeee547dae664d63071ec6b97d7d137f
Odd Stråbø <oddstr13@openshell.no>
2024-09-25 19:58:37 +0200

    Fix strings missed in rename

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "BackendAPI.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "BackendAPI.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
