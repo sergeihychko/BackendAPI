526c4e33cd94afb23848b4d087ee8e3a33649edf
Odd Stråbø <oddstr13@openshell.no>
2024-11-04 17:48:48 +0100

    Change indentation to repsect new editorconfig settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if [ "$DJANGO_DATABASE_ENGINE" = "django.db.backends.postgresql" ]; then
-    echo "Waiting for postgres..."
-    while ! nc -z $DJANGO_DATABASE_HOST "${DJANGO_DATABASE_PORT:-5432}"; do
-        sleep 0.1
-    done
+	echo "Waiting for postgres..."
+	while ! nc -z $DJANGO_DATABASE_HOST "${DJANGO_DATABASE_PORT:-5432}"; do
+		sleep 0.1
+	done
 
-    echo "PostgreSQL is running chief!"
+	echo "PostgreSQL is running chief!"
 fi
 
 python manage.py migrate
