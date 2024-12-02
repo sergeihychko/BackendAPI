ba6d4d1694c5be5a0e4e8f7558f3ff2c234e5a20
Ivan Artamonov <61617258+HollowKaeden@users.noreply.github.com>
2024-12-02 20:37:19 +0300

    Remove trialing slashes for api paths
    Co-authored-by: Odd Stråbø <oddstr13@openshell.no>

--- backendapi/urls.py
+++ backendapi/urls.py
@@ -6,9 +6,9 @@ register_converter(converters.BinaryHexConverter, "hex")
 
 urlpatterns = [
     path("nfctag/<hex:tag>", views.scanned),
-    path("test/", views.api_get, name="api_get"),
+    path("test", views.api_get, name="api_get"),
     path("tickets/verify/<slug:ticket>",
          views.verify_ticket,
          name="verify_ticket"),
-    path("attendants/", views.create_attendant, name="create_attendant")
+    path("attendants", views.create_attendant, name="create_attendant")
 ]
